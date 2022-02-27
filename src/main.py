4a38e54f9a1a94329e096f4893e06e4dad97af8f
Mikey Christoferis Qu4rkz <dale.breuer@gmx.de>
2022-02-27 23:51:35 +0100

    start of gui programming

--- src/main.py
+++ src/main.py
@@ -3,8 +3,41 @@ import matplotlib as plt
 from tkinter import Tk, Label, Text, Button, Frame
 import util
 
-def gui(window):
-    pass
+def gui():
+
+    window = Tk()
+    window.title("Climate Diagrams by Christoferis")
+
+    #Frame for additional Info + TODO: Radiobutton to activate it
+    info = Frame(window)
+
+    #Weather Station
+    weather = Frame(info)
+    Label(weather, text="Weather Station / Country").pack()
+    w_place = Text(weather).pack(side="left")
+    w_cou = Text(weather).pack(side="right")
+    weather.pack()
+
+    #Coordinates, Elevation
+    extra = Frame(info)
+    Label(extra, text="Elevation").pack()
+    e_ele = Text(extra).pack()
+    Label(extra, text="Weather Station Coordinates").pack()
+    e_coo = Text(extra).pack()
+    extra.pack(side="bottom")
+
+    info.pack()
+
+    #TODO: Options
+    #include chart
+    #include Temperature, Rainfall Averages
+
+
+
+
+
+
+    window.mainloop()
 
 def plot(data):
     pass
